10eff8fddedca28a9a34cf8909610580420a5d40
TomTheMelonCat <56547000+TomTheMelonCat@users.noreply.github.com>
2022-06-22 21:14:44 +0200

    Added descriptions, fixed readme file

--- README.md
+++ README.md
@@ -1,25 +1,45 @@
-# Description
-This is a python script which can be used to solve simple square or rectangular mazes from .txt files.<br />
-Maze is defined as a text file, with dimensions X*Y (it can be a maze of any size) made of the following characters:<br />
-&emsp;**X** - wall (or any other chosen symbol defined as **'wall'** in configuration file)<br />
-&emsp;**' '** - road (or any other chosen symbol defined as **'path'** in configuration file)<br />
-&emsp;**0** - starting point (or any other chosen symbol defined as **'start'** in configuration file)<br />
-&emsp;**F** - finish (or any other chosen symbol defined as **'finish'** in configuration file)<br /><br />
-The program in Python is able to find the shortest path through the maze from the 'start' character and finishing at the 'finish'.
-The shortest path found is printed on the screen.<br />
+# Project Title
 
-# Installation
-1, Clone git repository via **git clone https://github.com/TomTheMelonCat/maze.git**<br />
-2, Install Python pip package :<br />
-&emsp; - download [Pip](https://bootstrap.pypa.io/get-pip.py) package <br />
-&emsp; - powershell -> **path/to/pip/folder python get-pip.py** <br />
-&emsp; - check whether installation was successful via displaying version of pip (pip -V) <br />
-3, **Requirements.txt** has to be installed with pip command (python -m pip install requirements.txt) <br />
-# Configuration
-1, In order to make script work correctly, valid values has to be set in the configuration file - config.yaml.<br />
-&emsp;Default values have been set, so you can take them as a template and change them accordingly. <br />
-2, In order to have the script working correctly, you have to have a maze in a .txt file. <br />
-&emsp;Two default mazes are a part of the repository. Keep in mind, that mazes have to be in X*Y format <br />
-&emsp;and must only contain valid symbols as specified in config.yaml. <br />
-# Run
-1, Run powershell -> **path/to/script python maze_solver.py** OR run **maze_solver.py** in a chosen IDE.
+Maze solver
+
+## Description
+
+This is a python script which can be used to solve simple square or rectangular mazes from .txt files. <br />
+Maze is defined as a text file, with dimensions X*Y (it can be a maze of any size) made of the following characters:<br />
+
+* X - wall (or any other chosen symbol defined as *'wall'* in configuration file)
+* ' ' - road (or any other chosen symbol defined as *'path'* in configuration file)
+* 0 - starting point (or any other chosen symbol defined as *'start'* in configuration file)
+* F - finish (or any other chosen symbol defined as *'finish'* in configuration file)
+
+The program in Python is able to find the shortest path through the maze from the 'start' character and finishing at the 
+'finish'. The shortest path found is printed on the screen.
+
+## Getting Started
+
+### Dependencies
+
+* Python version 3.7 and higher required
+* Python pip package
+* Requirements.txt has to be installed via `python -m pip install requirements.txt`
+
+### Installing
+
+* Clone git repository via `git clone https://github.com/TomTheMelonCat/maze.git`
+
+### Executing program
+
+* Program can be run in any chosen IDE
+* Second option is to run the script straight from CMD or PowerShell via `path/to/script python maze_solver.py`
+
+## Author
+
+*Jiří Kadlec*  
+[@GitHub](https://github.com/TomTheMelonCat/)
+
+## Version History
+
+* 0.2
+    * Various bug fixes and optimizations/descriptions
+* 0.1
+    * Initial Release
--- maze_solver.py
+++ maze_solver.py
@@ -1,17 +1,25 @@
-from itertools import count
-from operator import truediv
 import os
 import yaml
 from sys import stderr
 
 
-def maze_check(_maze):
-    dims = [len(_maze), len(_maze[0])]
-    count_check = dims[0]*dims[1]
+def maze_check(maze):
+    '''
+    Check the maze for errors or invalid symbols.
+
+    :param maze: A 2d array containing the maze.
+    '''
+    dimensions = [len(maze), len(maze[0])]
+    count_check = dimensions[0] * dimensions[1]
     symbol_count = 0
     symbols = load_config('symbols')
-    for row in _maze:
-        if row != dims:
+    for symbol in symbols.values():
+        if len(symbol) > 1 or len(symbol) == 0:
+            print('Some of the specified symbols are invalid. Please check your config file.', file=stderr)
+            exit(-1)
+
+    for row in maze:
+        if row != dimensions:
             pass
         for col in row:
             symbol_count += 1
@@ -19,40 +27,68 @@ def maze_check(_maze):
                 continue
             else:
                 print('Invalid symbols found in maze.', file=stderr)
-                exit(0)
+                exit(-1)
+
     if symbol_count != count_check:
         print('Invalid maze dimensionality.', file=stderr)
-        exit(0)
+        exit(1)
 
 
-def load_config(mode:str=None):
+def load_config(specified_param = None):
+    '''
+    Loads configuration file.
+
+    :param specified_param: Is either None by default, returning the entire configuration file, or
+    has a parameter specified, defining what part of the configuration file to return. 
+    '''
     with open('config.yaml', 'r') as r:
-        conf = yaml.safe_load(r)
+        config = yaml.safe_load(r)
         r.close()
-    if mode is None:
-        return conf
+    
+    if specified_param is None:
+        return config
     else:
         try:
-            return conf[mode]
-        except:
-            print('Invalid dict key.', file=stderr)
-            exit(0)
+            return config[specified_param]
+        except KeyError as err:
+            print(f'Invalid dict key - {err}', file=stderr)
+            exit(1)
 
 
 def print_result(solved_maze, raw_maze, path_len):
-    shortest_path_symbol = load_config('shortest_path_symbol')
+    '''
+    Prints original and modified (with shortest path) maze. Also prints a message containing how many
+    steps are in an actual shortest path.
+
+    :param solved_maze: Modified maze containing the shortest path. 
+    :param raw_maze: Original unchanged maze. 
+    :param path_len: Length of the shortest path. 
+    '''
+    symbols = load_config('symbols')
+    shortest_path_symbol = symbols['shortest_path_symbol']
     print('Original maze vs solved maze:\n')
     for row in zip(solved_maze, raw_maze):
         joined_row = ''.join(row[1].rstrip('\n')) + '\t' + '|' + '\t' + ''.join(row[0])
         print(joined_row)
+
     print(f'\nMaze solved - path is represented by "{shortest_path_symbol}" symbol defined in configuration file.' \
            f' Length of the shortest path is {path_len}.')
 
 
 def forge_path(start, finish, travelled_path, maze):
+    '''
+    Forges (constructs) the shortest path and inserts it into the original maze.
+
+    :param start: Start of the maze. 
+    :param finish: Finish of the maze. 
+    :param travelled_path: The path which has been pivotted through. 
+    :param maze: A 2d array containing the maze. 
+
+    :return: Shortest path and a maze in which has been the shortest path marked by a specified symbol.
+    '''
     shortest_path = []
     symbols = load_config('symbols')
-    shortest_path_symbol = load_config('shortest_path_symbol')
+    shortest_path_symbol = symbols['shortest_path_symbol']
     for idx in range(len(travelled_path.keys())):
         for point in travelled_path.items():
             if point[0] == finish:
@@ -63,14 +99,23 @@ def forge_path(start, finish, travelled_path, maze):
                 break
             else:
                 continue
+
         if finish == start:
             break
     
     return shortest_path, maze
 
 
-def prep_maze(maze:list):
-    _maze = []
+def prep_maze(maze):
+    '''
+    Removes trailing endlines from each of the lines of a particular maze, checks the maze for errors or
+    undefined symbols and defines start and finish.
+
+    :param maze: A 2d array containing the maze. 
+
+    :return: A maze checked through, start and finish
+    '''
+    maze_ = []
     symbols = load_config('symbols')
     start, finish = (), ()
     for line in maze:
@@ -78,36 +123,46 @@ def prep_maze(maze:list):
         row = []
         for letter in line:
             row.append(letter)
-        _maze.append(row)
-    maze_check(_maze)
+        maze_.append(row)
 
-    for row in _maze:
+    maze_check(maze_)
+    for row in maze_:
         for symbol in row:
             if symbol == symbols['finish']:
-                finish = (_maze.index(row), row.index(symbol))
+                finish = (maze_.index(row), row.index(symbol))
             elif symbol == symbols['start']:
-                start = (_maze.index(row), row.index(symbol))
-    return _maze, start, finish
+                start = (maze_.index(row), row.index(symbol))
+
+    return maze_, start, finish
 
 
 def load_maze(path):
+    '''
+    Loads maze from a specified path
+
+    :param path: A string path of the maze. 
+    '''
     if os.path.exists(path):
         with open(path, 'r') as r:
             maze = r.readlines()
     else:
         print('Invalid path.', file=stderr)
-        exit(0)
+        exit(1)
     return maze
 
 
 def solve(path):
+    '''
+    Solves a .txt maze by finding the shortest path.
+
+    :param path: A string path of the maze. 
+    '''
     symbols = load_config('symbols')
     raw_maze = load_maze(path)
     maze, start, finish = prep_maze(raw_maze)
     queue = [start]
     mapped = [start]
     travelled_path = {}
-
     while queue is not None:
         current_pos = queue.pop(0)
         if current_pos == finish:
